f948b4622a5664256f95f2c37f3a8faf48e37125
Divyadvtp <tailord662@gmail.com>
2023-01-23 15:19:14 -0500

    my first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import Navbar from './components/Navbar'
+import Header from './components/Header'
+import Projects from './components/Projects'
+import About from './components/About'
+import Contact from './components/Contact'
+import Footer from './components/Footer'
+
+
+
+function App() {
+  return (
+    <div>
+        <Navbar />
+        <Header />
+        <Projects />
+        <About />
+        <Contact />
+        <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+        <div className="footer">
+            <p>Developed By Divya Patel</p>
+        </div>
+    )
+}
+
+export default Footer
